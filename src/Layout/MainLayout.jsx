8db144628871d9225815190bcd7393dc3e05ab54
MxAziz <hereabdulaziz@gmail.com>
2025-02-06 16:46:08 +0600

    customize navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar";
 import Footer from "../Pages/Shared/Footer";
+import "../App.css"
 
 const MainLayout = () => {
   return (
-    <div className=" min-h-screen flex flex-col">
+    <div className=" min-h-screen flex flex-col bg-[#2E3440] ">
       <Navbar></Navbar>
       <div className="flex-grow">
         <Outlet></Outlet>
